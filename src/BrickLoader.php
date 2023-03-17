ca84249e8598e35c3b8d9b5a0580529c54d4e4ec
Kevin Traini <kevin@ktraini.com>
2023-03-17 14:08:09 +0100

    Begin BrickLoader

--- src/BrickLoader.php
+++ src/BrickLoader.php
@@ -25,9 +25,90 @@
 
 namespace Marmot\Brick;
 
+use Composer\ClassMapGenerator\ClassMapGenerator;
+use Composer\InstalledVersions;
+use Marmot\Brick\Events\Event;
+use Marmot\Brick\Exceptions\PackageContainsNoBrickException;
+use Marmot\Brick\Exceptions\PackageContainsSeveralBrickException;
+use Marmot\Brick\Services\Service;
+use ReflectionClass;
+use ReflectionException;
+
 /**
  * This class is used to load all Bricks with their Events and Services
  */
 final class BrickLoader
 {
+    private const PACKAGE_TYPE = 'marmot-brick';
+
+    /**
+     * Load all installed Bricks
+     *
+     * @throws PackageContainsSeveralBrickException
+     * @throws PackageContainsNoBrickException
+     */
+    public function loadBricks(): void
+    {
+        $packages = InstalledVersions::getInstalledPackagesByType(self::PACKAGE_TYPE);
+
+        foreach ($packages as $package) {
+            try {
+                $this->loadBrick($package);
+            } catch (ReflectionException) {
+                // Ignore ReflectionException, but let pass others
+            }
+        }
+    }
+
+    /**
+     * @param string $package
+     * @return void
+     * @throws PackageContainsSeveralBrickException
+     * @throws ReflectionException
+     * @throws PackageContainsNoBrickException
+     */
+    private function loadBrick(string $package): void
+    {
+        $install_path = InstalledVersions::getInstallPath($package);
+        if ($install_path === null) {
+            return; // Package is not installed, ignore it
+        }
+
+        $map = ClassMapGenerator::createMap($install_path);
+
+        /** @var ReflectionClass<Brick>|null $brick */
+        $brick     = null;
+        $_services = []; // TODO : remove the _, it's here just for silent psalm
+        $_events   = [];
+
+        foreach ($map as $symbol => $_path) { // For each class in Brick package
+            $ref = new ReflectionClass($symbol);
+
+            if ($ref->isSubclassOf(Brick::class)) { // If class is Brick
+                if ($brick === null) {
+                    $brick = $ref;
+                    continue;
+                } else {
+                    throw new PackageContainsSeveralBrickException($package, $brick->getName(), $ref->getName());
+                }
+            }
+
+            $attrs = $ref->getAttributes(Service::class);
+            if (!empty($attrs)) { // If class is Service
+                $_services[] = $ref;
+                continue;
+            }
+
+            $attrs = $ref->getAttributes(Event::class);
+            if (!empty($attrs)) { // If class is Event
+                $_events[] = $ref;
+            }
+        }
+
+        if ($brick === null) {
+            throw new PackageContainsNoBrickException($package);
+        }
+
+        // TODO : store a Brick reference
+    }
 }
